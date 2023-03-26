a9bc025a0f052d28937f4e84e4ee832ee7eced2a
husnuyasar <husnuyasar58@gmail.com>
2023-03-26 18:00:23 +0300

    :recycle: Refactored index.js file to clean up the code structure and improve readability

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, createHttpLink } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
+import { client } from "./apolloClient"
+import './index.css';
+
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:8080/graphql'
+// })
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem("access_token");
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization :token ? `Bearer ${token}` : "",
+//     }
+//   }
+// })
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+// client
+//   .query({
+//     query: gql`
+//         query getAll($status: Float!) {
+//             getAll(status: $status) {Id,Description,Checked}
+//         }
+//     `,
+//     variables: {status : 0}
+//   })
+//   .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
